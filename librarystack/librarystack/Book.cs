4628a1d78a1bcfa4616b8d5cdf9187c667bae1e7
benjaminbhr <56117081+benjaminbhr@users.noreply.github.com>
2020-03-04 21:44:15 +0100

    Add files via upload

---
+++ librarystack/librarystack/Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace librarystack
+{
+    public class Book
+    {
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private string genre;
+
+		public string Genre
+		{
+			get { return genre; }
+			set { genre = value; }
+		}
+
+		private int pages;
+
+		public int Pages
+		{
+			get { return pages; }
+			set { pages = value; }
+		}
+
+		private string author;
+
+		public string Author
+		{
+			get { return author; }
+			set { author = value; }
+		}
+
+		public Book(string name,string genre,int pages,string author)
+		{
+			this.Name = name;
+			this.Genre = genre;
+			this.Pages = pages;
+			this.Author = author;
+		}
+
+
+
+
+	}
+}
---
+++ librarystack/librarystack/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace librarystack
+{
+    class Program
+    {
+        static ConsoleKeyInfo UserInput()
+        {
+            ConsoleKeyInfo cki;
+            cki = Console.ReadKey(true);
+            return cki;
+        }
+        static void Main(string[] args)
+        {
+            List<Book> booksInLib = new List<Book>();
+            booksInLib.Add(new Book("Fire and Blood","Fantasy",736,"George R. R. Martin"));
+            booksInLib.Add(new Book("A Storm of Swords", "Fantasy", 973, "George R. R. Martin"));
+            booksInLib.Add(new Book("A Clash of Kings", "Fantasy", 761, "George R. R. Martin"));
+            booksInLib.Add(new Book("A Game of Thrones", "Fantasy", 694, "George R. R. Martin"));
+            Stack lendbooks = new Stack();
+            bool showmenu = true;
+            while (showmenu)
+            {
+                Console.Clear();
+                Console.WriteLine("===============");
+                Console.WriteLine("    Library");
+                Console.WriteLine("===============");
+                Console.WriteLine("(1) Add Books\n(2) Show book stack\n(3) Exit");
+                switch (UserInput().Key)
+                {
+                    case ConsoleKey.D1:
+                        if (booksInLib.Count == 0)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("There are no books left in the library!");
+                            Console.WriteLine("\nPress enter to return to main menu!");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.Write($"Please select a book\n");
+                            for (int i = 0; i < booksInLib.Count; i++)
+                            {
+                                Console.WriteLine($"({(i+1)}){booksInLib.ElementAt(i).Name}\n");
+                            }
+                            switch (UserInput().Key)
+                            {
+                                case ConsoleKey.D1:
+                                    lendbooks.Push(booksInLib.ElementAt(0));
+                                    booksInLib.RemoveAt(0);
+                                    break;
+                                case ConsoleKey.D2:
+                                    lendbooks.Push(booksInLib.ElementAt(1));
+                                    booksInLib.RemoveAt(1);
+                                    break;
+                                case ConsoleKey.D3:
+                                    lendbooks.Push(booksInLib.ElementAt(2));
+                                    booksInLib.RemoveAt(2);
+                                    break;
+                                case ConsoleKey.D4:
+                                    lendbooks.Push(booksInLib.ElementAt(3));
+                                    booksInLib.RemoveAt(3);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        
+                        break;
+                    case ConsoleKey.D2:
+                        Console.Clear();
+                        bool caserunning = true;
+                        while (lendbooks.Count > 0 && caserunning)
+                        {
+                            Console.WriteLine("Finish loan");
+                            string temp_name = ((Book)lendbooks.Peek()).Name;
+                            Console.WriteLine($"Would you like to lend ({temp_name}) ----- ({lendbooks.Count}) Books left in your stack!\n(1)Yes\n(2)No");
+                            switch (UserInput().Key)
+                            {
+                                case ConsoleKey.D1:
+                                    lendbooks.Pop();
+                                    break;
+                                case ConsoleKey.D2:
+                                    caserunning = false;
+                                    break;
+                                default:
+                                    break;
+                            }
+                            Console.Clear();
+                        }
+                        if (lendbooks.Count == 0)
+                        {
+                            Console.WriteLine("You have no books left in your stack!");
+                            Console.WriteLine("\nPress enter to return to main menu!");
+                            Console.ReadLine();
+                        }
+                        break;
+                    case ConsoleKey.D3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
