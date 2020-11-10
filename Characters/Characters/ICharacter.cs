b6ee9e3326b4212dd1b358072eec8111b477e882
benjaminroesdal <53909855+benjaminroesdal@users.noreply.github.com>
2020-11-10 11:54:25 +0100

    Uploaded characters

---
+++ Characters/Characters/ICharacter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters
+{
+    interface ICharacter
+    {
+        void Die();
+        void Fight();
+        void Heal();
+    }
+}
---
+++ Characters/Characters/IWitch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters
+{
+    interface IWitch
+    {
+        void RaiseShield();
+        void ShieldGlare();
+        void Teleport(int x, int y);
+        void ThrowFrostNova();
+        void ThrowMagicMisile();
+    }
+}
