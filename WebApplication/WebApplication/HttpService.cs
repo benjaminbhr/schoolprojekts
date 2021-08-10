6cd2416299fb6aa20dc2f348a94980d341f4ce26
benjaminbhr <56117081+benjaminbhr@users.noreply.github.com>
2021-08-10 08:22:08 +0200

    added new school project

---
+++ WebApplication/WebApplication/HttpService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebApplication
+{
+    public static class HttpService
+    {
+        static readonly HttpClient client = new HttpClient();
+        private static string BaseUrl = "https://";
+
+        /// <summary>
+        /// Http Get request using the provided url together with the baseUrl
+        /// </summary>
+        /// <param name="url">User inputted url</param>
+        public static async Task<string> GetHttpResponse(string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(BaseUrl + url);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsByteArrayAsync();
+            string utfformatedResponse = Encoding.UTF8.GetString(responseBody);
+            string formattedResponseFinal = Regex.Replace(utfformatedResponse, "<[^>]*>", String.Empty);
+
+            return formattedResponseFinal;
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace WebApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Url:");
+                string requestUrl = Console.ReadLine();
+                var httpresponse = HttpService.GetHttpResponse(requestUrl).Result;
+                Console.WriteLine(httpresponse);
+                Console.ReadLine();
+            }
+        }
+    }
+}
