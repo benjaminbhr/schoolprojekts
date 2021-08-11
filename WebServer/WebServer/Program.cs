5919fd2a84fb3c03363cdcc9f7416a88c519eee4
benjaminbhr <56117081+benjaminbhr@users.noreply.github.com>
2021-08-11 05:15:37 +0200

    Updated webserver

--- WebServer/WebServer/Program.cs
+++ WebServer/WebServer/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace WebServer
 {
@@ -9,8 +11,20 @@ namespace WebServer
     {
         static void Main(string[] args)
         {
+            String strHostName = string.Empty;
+            // Getting Ip address of local machine...
+            // First get the host name of local machine.
+            strHostName = Dns.GetHostName();
+            Console.WriteLine("Local Machine's Host Name: " + strHostName);
+            // Then using host name, get the IP address list..
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+
+            var regex = new Regex(@"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+
+            var addr = ipEntry.AddressList.Where(e => true == regex.IsMatch(e.ToString()));
             WebServer server = new WebServer();
-            server.start(IPAddress.Parse("10.108.137.33"), 8080, 10, Directory.GetCurrentDirectory());
+
+            server.start(addr.FirstOrDefault(), 8080, 10, Directory.GetCurrentDirectory());
         }
     }
 }
