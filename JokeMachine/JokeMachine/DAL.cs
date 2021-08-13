31478e08131af37ddae076aff4fd02523b6c0b3c
benjaminbhr <56117081+benjaminbhr@users.noreply.github.com>
2021-08-13 08:11:28 +0200

    Pushed JokeMachine

---
+++ JokeMachine/JokeMachine/DAL.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace JokeMachine
+{
+    public class DAL
+    {
+        public List<Joke> dadJokes = new List<Joke>()
+        {
+            new Joke(){Setup = "Mine sko har lige forladt mig",Punchline = "– De følte sig bundet"},
+            new Joke(){Setup = "Hvad kalder man en postmands numse?",Punchline = "– Det en post-nummer"},
+            new Joke(){Setup = "Hvor gammel kan en elg blive?",Punchline = "– Ældgammel!"},
+            new Joke(){Setup = "Hvordan får man en fisk til at svømme ekstra hurtigt?",Punchline = "– Man tuner den"},
+            new Joke(){Setup = "Hvad hedder Draculas fætter, som i øvrigt er vegetar?",Punchline = "– Rucola"},
+            new Joke(){Setup = "Hvad laver tømreren når han langt om længe får fri fra arbejde?",Punchline = "– Lige vatterpass’er ham"},
+            new Joke(){Setup = "Hvordan bliver man ekspert i tordenvejr?",Punchline = "– Man tager et lynkursus"},
+            new Joke(){Setup = "Må jeg gerne spise yoghurt i sofaen?",Punchline = "– Far: “Nej put det i en skål?”"},
+            new Joke(){Setup = "Hvad gør man hvis AGF binder superligaen?",Punchline = "– Slukker for sin Playstation og vender tilbage til det virkelige liv!"},
+            new Joke(){Setup = "Hvad er det mindst talte sprog i verden? ",Punchline = "– Tegnsprog"},
+            new Joke(){Setup = "Hvad sagde den ene cykel til den anden?",Punchline = "– Styr dig!"},
+            new Joke(){Setup = "Hvad fik den lille kannibaldreng, som kom for sent til aftensmaden",Punchline = "– En kold skulder"},
+            new Joke(){Setup = "Hvad tager flyvermaskine på når den fryser",Punchline = "– Flyverdragt"},
+            new Joke(){Setup = "Hvilken slags slik kan bilister ikke lide?",Punchline = "– Lak-riser"},
+            new Joke(){Setup = "Hvad kalder man en fuld frø?",Punchline = "– en sprit tusse"},
+            new Joke(){Setup = "Hvordan siger man “spion-perler” på engelsk?",Punchline = "– Spy-perls (spegepølse)"},
+            new Joke(){Setup = "Min datter skreg “Faar hører du overhovedet efter?!!!?!”",Punchline = "– Det en mærkelig måde, at starte en samtale på..."},
+            new Joke(){Setup = "Hvorfor er katte så grimme?",Punchline = "– Det er fordi de misfarvede"},
+            new Joke(){Setup = "Mine sko har lige forladt mig",Punchline = "– De følte sig bundet"},
+            new Joke(){Setup = "Hvad kalder man en postmands numse?",Punchline = "– Det en post-nummer"},
+            new Joke(){Setup = "Hvor gammel kan en elg blive?",Punchline = "– Ældgammel!"},
+            new Joke(){Setup = "Hvordan får man en fisk til at svømme ekstra hurtigt?",Punchline = "– Man tuner den"},
+            new Joke(){Setup = "Hvad hedder Draculas fætter, som i øvrigt er vegetar?",Punchline = "– Rucola"},
+            new Joke(){Setup = "Hvad laver tømreren når han langt om længe får fri fra arbejde?",Punchline = "– Lige vatterpass’er ham"},
+            new Joke(){Setup = "Hvordan bliver man ekspert i tordenvejr?",Punchline = "– Man tager et lynkursus"},
+            new Joke(){Setup = "Må jeg gerne spise yoghurt i sofaen?",Punchline = "– Far: “Nej put det i en skål?”"},
+            new Joke(){Setup = "Hvad gør man hvis AGF binder superligaen?",Punchline = "– Slukker for sin Playstation og vender tilbage til det virkelige liv!"},
+        };
+    }
+}
---
+++ JokeMachine/JokeMachine/JokeHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace JokeMachine
+{
+    public class JokeHandler
+    {
+        private DAL dal = new DAL();
+        public List<Joke> JokeListBasedOnCategory(EJokeCategory jokeCategory)
+        {
+            switch (jokeCategory)
+            {
+                case EJokeCategory.Dadjokes: return dal.dadJokes;
+            }
+            return null;
+        }
+
+        public Joke GetRandomJoke(List<Joke> alrdyRecievedJokes,EJokeCategory jokeCategory,string jokeLang)
+        {
+            var tempList = JokeListBasedOnCategory(jokeCategory);
+            var cleanedList = tempList.Intersect(alrdyRecievedJokes).ToList();
+
+            var rndJoke = cleanedList.ElementAt(RandomJokeNumber(cleanedList.Count));
+
+            return rndJoke;
+        }
+
+        public int RandomJokeNumber(int listLength)
+        {
+            var random = new Random();
+            var rnd = random.Next(0, listLength);
+            return rnd;
+        }
+
+
+    }
+}
