24d11cb7263cdf9f5d0b4fa1cbdda352736bd1b0
benjaminbhr <56117081+benjaminbhr@users.noreply.github.com>
2021-08-25 12:45:52 +0200

    AirTravelProj

---
+++ AirTravelConsole/AirTravelConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTravelConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new AirTravelEntities())
+            {
+                //Gets all routes with owner id of 1
+                var routes = context.Routes.Where(e=>e.Owner == 1).ToList();
+                //Gets all airports with IATA equal to LAX
+                var airport = context.Airports.Where(e=>e.IATA.Equals("LAX")).ToList();
+                //Gets all airlines with code - SAS
+                var airlines = context.Airlines.Where(e=>e.Code.Equals("SAS")).ToList();
+                Console.ReadKey();
+            }
+        }
+    }
+}
